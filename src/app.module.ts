0feef8df470d742661810b57412e302fff91f51e
Golldan <cristiano.henriqueferreirabp@gmail.com>
2022-04-25 21:18:17 -0300

    feat: Initial architecture with Prisma and database implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DomainModule } from './domain/domain.module';
+import { PresentationModule } from './presentation/presentation.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PresentationModule, DomainModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
