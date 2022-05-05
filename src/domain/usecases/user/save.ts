0c1ba33a027c0d59ac340c8798b709f034928cb7
Golldan <cristiano.henriqueferreirabp@gmail.com>
2022-05-05 20:30:42 -0300

    feat: Added passwords and hashs to users

--- src/domain/usecases/user/save.ts
+++ src/domain/usecases/user/save.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { User } from 'src/domain/models/user.model';
+import * as bcrypt from 'bcryptjs';
+import { Salt } from 'src/shared/constants/salt';
 
 const prisma = new PrismaClient();
 
@@ -8,12 +10,17 @@ const prisma = new PrismaClient();
 export class SaveUser {
   async call(user: User): Promise<any> {
     try {
+      const hashedPassword = bcrypt.hashSync(user.password, Salt);
+
       return prisma.user.create({
         data: {
           name: user.name,
           email: user.email,
+          password: hashedPassword,
         },
       });
-    } catch (error) {}
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 }
